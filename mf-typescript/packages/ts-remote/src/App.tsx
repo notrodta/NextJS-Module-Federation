01fb7a54367b521f272baf5fa0f68ec599ba6b49
Rod Ta <rody101@gmail.com>
2023-02-10 19:29:04 -0500

    Initial project commit

---
+++ mf-typescript/packages/ts-remote/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import getList from "./pokemonList";
+import PokemonListView from "./PokemonListView";
+
+import "./index.css";
+
+
+const App = () => (
+    <div>
+        <h1>TS-REMOTE</h1>
+        <PokemonListView list={getList("Bulb")} />
+    </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
